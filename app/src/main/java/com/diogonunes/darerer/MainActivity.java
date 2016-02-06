73adee384f7f86bf75481cf0d376be4276ac8876
Diogo Nunes <dialex.nunes@gmail.com>
2016-02-06 22:00:22 +0000

    add logic to buttons; use proper names;

--- app/src/main/java/com/diogonunes/darerer/MainActivity.java
+++ app/src/main/java/com/diogonunes/darerer/MainActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = DiceBox.class.getSimpleName();
@@ -21,31 +21,46 @@ public class MainActivity extends AppCompatActivity {
         initializeViews();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private void initializeViews() {
         _btnFAB = (FloatingActionButton) findViewById(R.id.fab);
     }
 
     // Event Handling
+
+    public void onClickAcceptChallenge(View view) {
+        TextView txtDecision = (TextView) findViewById(R.id.challenge_decision);
+        txtDecision.setText(R.string.challenge_accepted);
+    }
+
+    public void onClickDenyChallenge(View view) {
+        TextView txtDecision = (TextView) findViewById(R.id.challenge_decision);
+        txtDecision.setText(R.string.challenge_denied);
+    }
+
+    public void onClickConsiderChallenge(View view) {
+        TextView txtDecision = (TextView) findViewById(R.id.challenge_decision);
+        txtDecision.setText(R.string.challenge_considered);
+    }
 }
