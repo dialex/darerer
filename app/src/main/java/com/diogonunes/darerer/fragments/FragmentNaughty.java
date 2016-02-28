ad8eb283f5ee43689bc869c5a8e29458a4b68fa1
Diogo Nunes <dialex.nunes@gmail.com>
2016-02-28 19:24:39 +0000

    WARNING: for some reason, every fall apart with this commit; clean unused code on Naughty mode;

--- app/src/main/java/com/diogonunes/darerer/fragments/FragmentNaughty.java
+++ app/src/main/java/com/diogonunes/darerer/fragments/FragmentNaughty.java
@@ -1,24 +1,20 @@
 package com.diogonunes.darerer.fragments;
 
 import android.content.res.ColorStateList;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.diogonunes.darerer.R;
 import com.diogonunes.darerer.StringRoulette;
-import com.diogonunes.darerer.Utils;
 
 public class FragmentNaughty extends Fragment {
     private static final String LOG_TAG = FragmentNaughty.class.getSimpleName();
@@ -71,28 +67,7 @@ public class FragmentNaughty extends Fragment {
         cardChallenge.setText(challengeDesc);
 
         // Allows the user to decide
-        setDecision(getView(), 0);
         showChallengeText();
-    }
-
-    public void onClickAcceptChallenge(View view) {
-        setDecision(view, R.id.btn_naughty_challenge_yes);
-    }
-
-    public void onClickDenyChallenge(View view) {
-        setDecision(view, R.id.btn_naughty_challenge_no);
-
-        if (Utils.getRandomBool(30)) {
-            Snackbar snackbar = Snackbar
-                    .make(view, _encouragementsRoulette.roll(), Snackbar.LENGTH_INDEFINITE)
-                    .setAction(R.string.dialog_action_sorry, new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            // close self
-                        }
-                    });
-            snackbar.show();
-        }
     }
 
     // Auxiliary
@@ -102,8 +77,6 @@ public class FragmentNaughty extends Fragment {
     }
 
     private void initFragment() {
-        initButtonHandlers();
-
         // Instance variables
         _layoutDefault = (LinearLayout) getView().findViewById(R.id.layout_challenge_off);
         _layoutChallenge = (LinearLayout) getView().findViewById(R.id.layout_challenge_on);
@@ -118,24 +91,6 @@ public class FragmentNaughty extends Fragment {
         _encouragementsRoulette = new StringRoulette(encouragements);
     }
 
-    private void initButtonHandlers() {
-        Button btnYes = (Button) getView().findViewById(R.id.btn_kind_challenge_yes);
-        btnYes.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                onClickAcceptChallenge(v);
-            }
-        });
-
-        Button btnNo = (Button) getView().findViewById(R.id.btn_kind_challenge_no);
-        btnNo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View v) {
-                onClickDenyChallenge(v);
-            }
-        });
-    }
-
     private void showWelcomeText() {
         _layoutDefault.setVisibility(View.VISIBLE);
         _layoutChallenge.setVisibility(View.GONE);
@@ -144,29 +99,6 @@ public class FragmentNaughty extends Fragment {
     private void showChallengeText() {
         _layoutDefault.setVisibility(View.GONE);
         _layoutChallenge.setVisibility(View.VISIBLE);
-    }
-
-    private void setDecision(View view, int decision) {
-        int decisionText;
-        Drawable decisionImage;
-
-        switch (decision) {
-            case R.id.btn_naughty_challenge_yes:
-                decisionText = R.string.challenge_accepted;
-                decisionImage = ContextCompat.getDrawable(view.getContext(), R.drawable.img_meme_yes);
-                break;
-            case R.id.btn_naughty_challenge_no:
-                decisionText = R.string.challenge_denied;
-                decisionImage = ContextCompat.getDrawable(view.getContext(), R.drawable.img_meme_no);
-                break;
-            default:
-                decisionText = R.string.challenge_considered;
-                decisionImage = ContextCompat.getDrawable(view.getContext(), R.drawable.img_meme_maybe);
-                break;
-        }
-
-        _txtDecision.setText(decisionText);
-        _imgDecision.setImageDrawable(decisionImage);
     }
 
     private void setTheme() {
