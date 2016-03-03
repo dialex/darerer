e6c74168c4a38fb2f6fcb6cb0d6c05d7f5b29e3f
Diogo Nunes <dialex.nunes@gmail.com>
2016-03-03 22:32:59 +0000

    remove useless back back button from action bar

--- app/src/main/java/com/diogonunes/darerer/activities/MainActivity.java
+++ app/src/main/java/com/diogonunes/darerer/activities/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.SparseArray;
@@ -34,8 +33,6 @@ public class MainActivity extends AppCompatActivity {
         initActivity();
 
         setSupportActionBar(_toolbar);
-        ActionBar supportActionBar = getSupportActionBar();
-        if (supportActionBar != null) supportActionBar.setDisplayHomeAsUpEnabled(true);
 
         initViewPager(_viewPager);
         _tabLayout.setupWithViewPager(_viewPager);
