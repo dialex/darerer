cc541603c5af354da17094db36078ee1f189c513
Diogo Nunes <dialex.nunes@gmail.com>
2016-03-03 22:29:34 +0000

    recover settings default button

--- app/src/main/java/com/diogonunes/darerer/activities/MainActivity.java
+++ app/src/main/java/com/diogonunes/darerer/activities/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.SparseArray;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.diogonunes.darerer.R;
@@ -40,6 +42,28 @@ public class MainActivity extends AppCompatActivity {
 
         //displayTabIcons();
         _tabLayout.getTabAt(1).select(); //default tab
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     // Event Handling
