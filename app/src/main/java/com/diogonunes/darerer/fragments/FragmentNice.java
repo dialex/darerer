b2ba53b86155c54f8e18ef22d2ec5f87b601d852
Diogo Nunes <dialex.nunes@gmail.com>
2016-03-03 16:06:26 +0000

    get resources from nice actions and modifiers

--- app/src/main/java/com/diogonunes/darerer/fragments/FragmentNice.java
+++ app/src/main/java/com/diogonunes/darerer/fragments/FragmentNice.java
@@ -1,10 +1,12 @@
 package com.diogonunes.darerer.fragments;
 
 import android.content.res.ColorStateList;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,9 +20,10 @@ import com.diogonunes.darerer.Utils;
 
 public class FragmentNice extends Fragment {
     private static final String LOG_TAG = FragmentNice.class.getSimpleName();
-    private static StringRoulette _niceActsRoulette, _encouragementsRoulette;
+    private static StringRoulette _niceActionsRoulette, _niceModifiersRoulette, _encouragementsRoulette;
 
     private FloatingActionButton _fab;
+    private CardView _cardChallenge;
     private TextView _txtDecision;
     private ImageView _imgDecision;
     private LinearLayout _layoutDefault, _layoutChallenge;
@@ -44,9 +47,7 @@ public class FragmentNice extends Fragment {
     public void onStart() {
         super.onStart();
         initFragment();
-
-//        _layoutDefault.setVisibility(View.VISIBLE);
-//        _layoutChallenge.setVisibility(View.GONE);
+        showWelcomeText();
     }
 
     @Override
@@ -70,21 +71,39 @@ public class FragmentNice extends Fragment {
     }
 
     private void initFragment() {
+        Resources resourcesRoot = getResources();
+
         // Instance variables
 //        _layoutDefault = (LinearLayout) getView().findViewById(R.id.layout_nice_challenge_off);
 //        _layoutChallenge = (LinearLayout) getView().findViewById(R.id.layout_nice_challenge_on);
-//        _txtDecision = (TextView) getView().findViewById(R.id.txt_challenge_decision);
-//        _imgDecision = (ImageView) getView().findViewById(R.id.img_meme_decision);
+//        _txtDecision = (TextView) getView().findViewById(R.id.txt_kind_challenge_decision);
+//        _imgDecision = (ImageView) getView().findViewById(R.id.img_kind_meme_decision);
 
-        String[] niceChallenges = getResources().getStringArray(R.array.nice_challenges);
-        _niceActsRoulette = new StringRoulette(niceChallenges);
+        String[] niceActions = resourcesRoot.getStringArray(R.array.nice_challenges_actions);
+        _niceActionsRoulette = new StringRoulette(niceActions);
 
-        String[] encouragements = getResources().getStringArray(R.array.encouragements);
+        String[] niceModifiers = resourcesRoot.getStringArray(R.array.nice_challenges_modifiers);
+        _niceModifiersRoulette = new StringRoulette(niceModifiers);
+
+        String[] encouragements = resourcesRoot.getStringArray(R.array.encouragements);
         _encouragementsRoulette = new StringRoulette(encouragements);
     }
 
+    private void showWelcomeText() {
+        _layoutDefault.setVisibility(View.VISIBLE);
+        _layoutChallenge.setVisibility(View.GONE);
+    }
+
+    private void showChallengeText() {
+        _layoutDefault.setVisibility(View.GONE);
+        _layoutChallenge.setVisibility(View.VISIBLE);
+    }
+
     private void setTheme() {
+        int themeColor = ContextCompat.getColor(getContext(), R.color.colorNicePrimary);
+
         _fab.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_face_white));
-        _fab.setBackgroundTintList(ColorStateList.valueOf(ContextCompat.getColor(getContext(), R.color.colorNicePrimary)));
+        _fab.setBackgroundTintList(ColorStateList.valueOf(themeColor));
+        _cardChallenge.setBackgroundColor(themeColor);
     }
 }
