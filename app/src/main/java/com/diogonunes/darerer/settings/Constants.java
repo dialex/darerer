08641b0a35d16dfd5bfa8b6b3680021f6feb2245
Diogo Nunes <dialex.nunes@gmail.com>
2016-03-11 15:29:13 +0000

    add TODO

--- app/src/main/java/com/diogonunes/darerer/settings/Constants.java
+++ app/src/main/java/com/diogonunes/darerer/settings/Constants.java
@@ -1,5 +1,5 @@
 package com.diogonunes.darerer.settings;
 
 public class Constants {
-    public static final String APP_STORE_URL = "http://diogonunes.com/";
+    public static final String APP_STORE_URL = "http://diogonunes.com/";//TODO: replace with short url to store
 }
